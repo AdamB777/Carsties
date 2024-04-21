053e983eda6128a6107ed95f626963ed5d7de8a6
AdamB777 <akrupa777@gmail.com>
2024-04-21 20:07:04 +0200

    End of section 6

---
+++ src/API/SearchService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,17 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers
+{
+    public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+    {
+        public async Task Consume(ConsumeContext<AuctionFinished> context)
+        {
+            var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+            id
+        }
+    }
+}
